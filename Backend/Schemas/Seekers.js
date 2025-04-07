f0dd6b8e169f37db7b34891cdad541bba27c6be1
Anudeep0110 <anudeepgude765@gmail.com>
2025-04-07 21:56:46 +0530

    Seeker Schema Update

--- Backend/Schemas/Seekers.js
+++ Backend/Schemas/Seekers.js
@@ -10,6 +10,30 @@ const Seekers = new mongoose.Schema({
         required: true,
         unique: true,
     },
+    dob: {
+        type: Date,
+    },
+    gender: {
+        type: String,
+    },
+    linkedIn: {
+        type: String,
+        default: ''
+    },
+    jobPreferences: {
+        jobType:{
+            type: String,
+            required: true,
+        },
+        workAvailability:{
+            type: String,
+            required: true,
+        },
+        preferredLocation:{
+            type: String,
+            required: true,
+        },
+    },
     phoneNumber: {
         type: String,
         required: true,
@@ -40,6 +64,18 @@ const Seekers = new mongoose.Schema({
         startDate: { type: Date, required: true },
         endDate: { type: Date, required: true },
         location: { type: String, required: true }
+    }],
+    projects: [{
+        projectName: { type: String, required: true },
+        description: { type: String, required: true },
+        startDate: { type: Date, required: true },
+        endDate: { type: Date, required: true },
+    }],
+    certifications: [{
+        certificationName: { type: String, required: true },
+        issuingOrganization: { type: String, required: true },
+        issueDate: { type: Date, required: true },
+        expirationDate: { type: Date, required: true },
     }],
     portfolioLink: { type: String, default: '' },
     resumeLink: { type: String, default: '' },
